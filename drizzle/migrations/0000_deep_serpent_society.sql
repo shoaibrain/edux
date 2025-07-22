8d82c80adcf77e99c665ea57eefe8812ad31bb5e
Shoaib Rain <shoaibrain@Shoaibs-MacBook-Air-2.local>
2025-07-22 14:35:34 -0500

    nrn

--- drizzle/migrations/0000_deep_serpent_society.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "tenants" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"tenant_id" text NOT NULL,
-	"schema_name" text NOT NULL,
-	"name" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "tenants_tenant_id_unique" UNIQUE("tenant_id")
-);
